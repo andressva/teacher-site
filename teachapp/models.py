8406fc401100e587fb3db0d0d2a8968720a5372c
Andres Vergel Alvarez <amvergel@hotmail.com>
2020-06-02 08:38:08 -0500

    models created

--- teachapp/models.py
+++ teachapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=10)
+    subject = models.CharField(max_length=80)
+    degree = models.ForeignKey('Degree', on_delete=models.CASCADE)
+
+
+class Degree(models.Model):
+    code =  models.CharField(max_length=10)
+    name =  models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
